99ba62e641c7065e12c7bda52abe4e82dbf57826
Mike Dillon <mike@appropriate.io>
2019-07-22 20:09:02 -0700

    Add goldencheetah-beta cask

---
+++ Casks/goldencheetah-beta.rb
@@ -0,0 +1,14 @@
+cask 'goldencheetah-beta' do
+  version '3.5-DEV1903'
+  sha256 '3b14154f05ba75012debb0225bd2ebaba575047a1f9606a4a7a7f3ccbc780e13'
+
+  # github.com/GoldenCheetah/GoldenCheetah was verified as official when first introduced to the cask
+	url "https://github.com/GoldenCheetah/GoldenCheetah/releases/download/v#{version}/GoldenCheetah_v#{version}_64bit_MacOS.dmg"
+  appcast 'https://github.com/GoldenCheetah/GoldenCheetah/releases.atom'
+  name 'GoldenCheetah'
+  homepage 'https://www.goldencheetah.org/'
+
+	conflicts_with cask: :goldencheetah
+
+  app 'GoldenCheetah.app'
+end
